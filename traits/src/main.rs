86b0860ab6a2902a59797a64314363a0bd9108a2
Kevin Du <kkxandeer@gmail.com>
2019-12-31 11:47:01 +0800

    feat: typed trait document

---
+++ traits/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let number_list = vec![34, 50, 24, 100, 99];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['a', 'n', 'd', 'i'];
+
+    let result = largest(&char_list);
+    println!("The largest number is {}", result);
+}
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
