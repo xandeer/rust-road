f8a87d603a8a9e6454e69e6eb36fdff400ffd559
Kevin Du <kkxandeer@gmail.com>
2020-01-06 11:49:12 +0800

    feat: turn single-threaded server into multithreaded

--- hello-server/src/main.rs
+++ hello-server/src/main.rs
@@ -5,13 +5,18 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
 
+use hello::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("localhost:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
